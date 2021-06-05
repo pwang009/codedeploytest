cfbb68d6cb8ba379acb0be18660bcb5e0e61732d
Tony Wang <pwang009@gmail.com>
2021-06-04 22:22:02 -0700

    initial

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require 'vendor/autoload.php';
+
+use Carbon\Carbon;
+
+date_default_timezone_set('America/Los_Angeles'); 
+$date = Carbon::now();
+
+echo "server name is:   " . gethostname();
+echo "<br /> local time is:   " .  $date->toDayDateTimeString();
