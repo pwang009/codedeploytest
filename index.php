24d33d2f8cf92b298bd6d8cc6431550064030b1c
Tony Wang <pwang009@gmail.com>
2021-06-10 17:10:51 -0700

    minor changes

--- index.php
+++ index.php
@@ -10,4 +10,3 @@ echo "server name is:   " . gethostname();
 echo "<br /> local time is:   " .  $date->toDayDateTimeString();
 echo "<br /> Name is: Tony";
 echo "<br /> Name is: Faisal";
-echo "<br /> Name is: Siyea.net";
